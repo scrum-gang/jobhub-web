5ca576b3535fcef752d6e2ed7ed4bda7b79a818b
Suleman Malik <suleman.malik@mail.mcgill.ca>
2019-02-12 15:18:36 -0500

    feat: added more ui elements for resume upload page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./Features/Authentication/Login";
 import Register from "./Features/Authentication/Register";
 import Dashboard from "./Features/Dashboard/Dashboard";
+import UploadResume from "./Features/UploadResume/UploadResume";
 import { AuthProvider } from "./Shared/Authorization";
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route path="/" component={Dashboard} exact />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
+          <Route path="/upload-resume" component={UploadResume} />
           <Route />
         </Switch>
       </AuthProvider>
--- src/Features/UploadResume/UploadResume.tsx
+++ src/Features/UploadResume/UploadResume.tsx
@@ -1,27 +1,89 @@
-import { Card, createStyles, Theme, withStyles, WithStyles} from '@material-ui/core';
-import React from 'react';
+import * as React from "react";
+
+import {
+  Button,
+  createStyles,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  Paper,
+  TextField,
+  Theme,
+  Typography,
+  withStyles,
+  WithStyles
+} from "@material-ui/core";
+import { CloudUpload, Menu } from "@material-ui/icons";
+
+import { AuthRedirect, Protection } from "../../Shared/Authorization";
 
 const styles = (theme: Theme) =>
   createStyles({
-    
+    button: {
+      marginTop: 20
+    },
+    grid: {
+      height: "100vh",
+      padding: 0,
+      width: "100%"
+    },
+    icon: {
+      color: "#1e90ff" ,
+      fontSize: "inherit",
+      height: "auto",
+      margin: theme.spacing.unit,
+      width: "70%"
+    },
+    margin: {
+      margin: theme.spacing.unit
+    },
+    paper: {
+      height: "60%",
+      maxWidth: "300px",
+      padding: `${6 * theme.spacing.unit}px ${12 * theme.spacing.unit}px`,
+      width: "40%"
+    },
+    text: {
+      fontSize: "3vw"
+    },
+    textField: {
+      width: 250
+    }
   });
 
-export interface IProps extends WithStyles<typeof styles> { }
+interface IProps extends WithStyles<typeof styles> {}
 
-interface IState {
-  resume: string
-}
+const UploadResume: React.FunctionComponent<IProps> = ({ classes }) => {
+  return (
+    // <React.Fragment>
+    //   <AuthRedirect protection={Protection.LOGGED_IN} />
+    //     <h1>WIP</h1>
+    // </React.Fragment>
 
-class UploadResume extends React.Component<IProps, IState> {
-
-  public render() {
-    return (
-      <div>
-        <h1>upload</h1>
-      </div>
-    )
-  }
-  
-}
+    <Grid
+      container
+      className={classes.grid}
+      justify="center"
+      alignItems="center"
+    >
+      <Paper className={classes.paper}>
+        <Grid container justify="center">
+          <CloudUpload className={classes.icon}/>
+          <Typography component="h2" variant="h4" gutterBottom>
+            Drag and drop files
+          </Typography>
+          <Button
+            variant="contained"
+            color="default"
+            className={classes.button}
+          >
+            Browse
+          </Button>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+};
 
 export default withStyles(styles)(UploadResume);
