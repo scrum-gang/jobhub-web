fb3c184079add1928d2cfdb25d1e4e86362d81c5
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-02-19 03:12:39 -0500

    refactor: Extract endpoint strings into an enum

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -6,20 +6,30 @@ import UserType from "../config/types/accountTypes";
 import ILoginResponse from "../config/types/loginResponse";
 import IUser from "../config/types/user";
 
+enum UserEndpoints {
+  USERS = "/users",
+  SELF = "/users/self",
+  LOGIN = "/login",
+  REGISTER = "/signup",
+  RESEND_EMAIL = "/resend"
+}
+
 class UserAPI {
   constructor() {
-    api.createEntity("/users");
-    api.createEntity("/users/self");
-    api.createEntity("/login");
-    api.createEntity("/signup");
-    api.createEntity("/resend");
+    api.createEntity(UserEndpoints.USERS);
+    api.createEntity(UserEndpoints.SELF);
+    api.createEntity(UserEndpoints.LOGIN);
+    api.createEntity(UserEndpoints.REGISTER);
+    api.createEntity(UserEndpoints.RESEND_EMAIL);
   }
 
   public login = (payload: { email: string; password: string }) => {
-    return api.endpoints["/login"].create(payload).then(({ data }) => {
-      api.setJWT(data.token);
-      return data as ILoginResponse;
-    });
+    return api.endpoints[UserEndpoints.LOGIN]
+      .create(payload)
+      .then(({ data }) => {
+        api.setJWT(data.token);
+        return data as ILoginResponse;
+      });
   };
 
   public register = (payload: {
@@ -27,15 +37,15 @@ class UserAPI {
     password: string;
     type: UserType;
   }) => {
-    return api.endpoints["/signup"].create(payload);
+    return api.endpoints[UserEndpoints.REGISTER].create(payload);
   };
 
   public getSelf = () => {
-    return api.endpoints["/users/self"].getAll() as AxiosPromise<IUser>;
+    return api.endpoints[UserEndpoints.SELF].getAll() as AxiosPromise<IUser>;
   };
 
   public resendVerification = (payload: { email: string }) => {
-    return api.endpoints["/resend"].create(payload);
+    return api.endpoints[UserEndpoints.RESEND_EMAIL].create(payload);
   };
 }
 
