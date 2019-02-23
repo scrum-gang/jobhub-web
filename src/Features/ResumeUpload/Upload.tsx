df744071536811a621dc558e2bbf40dc5d63470f
Suleman Malik <suleman.malik@mail.mcgill.ca>
2019-02-23 00:47:25 -0500

    chore: reformat

--- src/Features/ResumeUpload/Upload.tsx
+++ src/Features/ResumeUpload/Upload.tsx
@@ -1,69 +1,63 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik } from "formik";
 
-import { 
-  Button,
-  Grid, 
-  Theme, 
-  withStyles 
-} from '@material-ui/core';
+import { Button, Grid, Theme, withStyles } from "@material-ui/core";
 
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-import Wrapper from './Wrapper';
+import Wrapper from "./Wrapper";
 
-import { SimpleFileUpload } from 'formik-material-ui';
+import { SimpleFileUpload } from "formik-material-ui";
 
 // 10 Megs
 const MAX_FILE_SIZE = 10485760;
 
 const schema = Yup.object().shape({
   file: Yup.mixed()
-    .required('Required')
+    .required("Required")
     .test(
-      'file',
-      'File must be less than 10MB',
+      "file",
+      "File must be less than 10MB",
       value => value == null || value.size < MAX_FILE_SIZE
-    ),
+    )
 });
 
 const styles = (theme: Theme) => ({
   formControl: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     margin: theme.spacing.unit,
     minWidth: 120
-  },
+  }
 });
 
 interface IValues {
-  file: '' | File;
+  file: "" | File;
 }
 
 export default withStyles(styles)(() => (
   <Wrapper title="Resume Upload">
-  <Grid justify="center">
-    <Formik<IValues>
-      validationSchema={schema}
-      initialValues={{ file: '' }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          setSubmitting(false);
-          // action('submit')(values);
-        }, 2000);
-      }}
-      render={({ submitForm }) => (
-        <Form>
-          <Field component={SimpleFileUpload} name="file" />
-          <br />
-          <Button variant="contained" color="primary" onClick={submitForm}>
-            Submit
-          </Button>
-          <br />
-          
-        </Form>
-      )}
-    />
-  </Grid>
+    <Grid justify="center">
+      <Formik<IValues>
+        validationSchema={schema}
+        initialValues={{ file: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+            // action('submit')(values);
+          }, 2000);
+        }}
+        render={({ submitForm }) => (
+          <Form>
+            <Field component={SimpleFileUpload} name="file" />
+            <br />
+            <Button variant="contained" color="primary" onClick={submitForm}>
+              Submit
+            </Button>
+            <br />
+          </Form>
+        )}
+      />
+    </Grid>
   </Wrapper>
 ));
